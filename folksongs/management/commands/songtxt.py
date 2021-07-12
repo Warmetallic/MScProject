3efee4e1066054dfe359e5d9ee54405b81134b44
Gleb Kharlamov <scream10@list.ru>
2021-07-12 16:39:25 +0100

    song page changed, collapse button, removed some text

--- folksongs/management/commands/songtxt.py
+++ folksongs/management/commands/songtxt.py
@@ -3,15 +3,15 @@ import pytesseract
 import os.path
 
 # convert all files ending in .jpg inside a directory
-dirname = "C:\copy\djangoproject\Alpha-2021\static\songs"
-i = 46
+dirname = "C:\\songs\\1517"
+i = 0
 text = []
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 # print(pytesseract.image_to_string(r'C:\copy\djangoproject\Alpha-2021\static\songs\GDFSC_v8_0048.tif'))
 
 with open('test.txt', 'w') as f:
-	f.write(pytesseract.image_to_string(r'C:\copy\djangoproject\Alpha-2021\static\songs\GDFSC_v8_0048.tif'))
+	f.write(pytesseract.image_to_string(r'C:\\songs\\1517\\15171.png'))
 
 f.close()
 
@@ -19,7 +19,7 @@ f.close()
 for fname in os.listdir(dirname):
 	i+=1
 
-	if not fname.endswith(".tif"):
+	if not fname.endswith(".png"):
 		continue
 	path = os.path.join(dirname, fname)
 	if os.path.isdir(path):
@@ -27,7 +27,7 @@ for fname in os.listdir(dirname):
 
 	with open('test.txt', 'a') as f:
 		f.write('PAGE START:============================================================\n')
-		f.write(pytesseract.image_to_string(r'C:\copy\djangoproject\Alpha-2021\static\songs\GDFSC_v8_00'+str(i)+'.tif'))
+		f.write(pytesseract.image_to_string(r'C:\\songs\\1517\\1517'+str(i)+'.png'))
 
 f.close()
 
